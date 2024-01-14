fa66b93742b80ea4bb25d79b0fa009d4f3aec780
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-13 20:34:22 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -1,66 +1,144 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var Log = ""
-    @State var Installed = UntetherInstalled()
-    @State var Enabled = FileManager.default.fileExists(atPath: UntetherEnabledPath)
+    @State var kfd: UInt64 = 0
+    @State var LogItems: [String.SubSequence] = [IsSupported() ? "Ready!" : "Unsupported", "iOS: \(GetiOSBuildID())"]
     var body: some View {
-        Form {
-            if !Log.isEmpty {
-                Section {
-                    Text(Log)
-                }
-            }
-            Section(footer: Text("Created by @AppInstalleriOS")) {
-                Button {
-                    if UntetherInstalled() {
-                        let Output = String(describing: spawnRoot(Bundle.main.executablePath ?? "", ["uninstall"]).dropLast())
-                        if !Output.isEmpty {
-                            Log = Output
-                        }
-                    } else {
-                        let Output = String(describing: spawnRoot(Bundle.main.executablePath ?? "", ["install"]).dropLast())
-                        if !Output.isEmpty {
-                            Log = Output
+        VStack {
+            ScrollView {
+                ScrollViewReader { scroll in
+                    VStack(alignment: .leading) {
+                        ForEach(0..<LogItems.count, id: \.self) { LogItem in
+                            Text("[*] \(String(LogItems[LogItem]))")
+                            .textSelection(.enabled)
+                            .font(.custom("Menlo", size: 15))
                         }
                     }
-                    Installed = UntetherInstalled()
-                    Enabled = FileManager.default.fileExists(atPath: UntetherEnabledPath)
-                } label: {
-                    Text("\(Installed ? "Uninstall" : "Install") Untether")
-                }
-                .disabled(!isJailbroken())
-                if Installed {
-                    Button {
-                        SetUntether(!FileManager.default.fileExists(atPath: UntetherEnabledPath))
-                        Enabled = FileManager.default.fileExists(atPath: UntetherEnabledPath)
-                    } label: {
-                        Text("\(Enabled ? "Disable" : "Enable") Untether")
+                    .onReceive(NotificationCenter.default.publisher(for: LogStream.shared.reloadNotification)) { obj in
+                        DispatchQueue.global(qos: .utility).async {
+                            FetchLog()
+                            scroll.scrollTo(LogItems.count - 1)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity, alignment: .leading)
             }
+            .padding()
+            .frame(width: UIScreen.main.bounds.width - 80, height: 300)
+            .background(Color(UIColor.systemGray6))
+            .cornerRadius(20)          
+            Button {
+                if kfd == 0 {
+                    LogStream.shared.pause()
+                    kfd = do_kopen(0x800, 0x0, 0x2, 0x2)
+                    LogStream.shared.resume()
+                    print("Done")
+                } else {
+                    print("a")
+                    do_kclose()
+                }
+            } label: {
+                Text(kfd == 0 ? "Exploit" : "Post Exploit")
+                .font(.system(size: 20))
+            }
+            .disabled(!IsSupported())
+            .buttonStyle(.plain)
+            .frame(width: UIScreen.main.bounds.width - 80, height: 70)
+            .background(Color(UIColor.systemGray6))
+            .cornerRadius(20)
         }
+    }
+    func FetchLog() {
+        guard let AttributedText = LogStream.shared.outputString.copy() as? NSAttributedString else {
+            LogItems = ["Error Getting Log!"]
+            return
+        }
+        LogItems = AttributedText.string.split(separator: "\n")
     }
 }
 
-let UntetherEnabledPath = "/var/mobile/.untether"
-
-func SetUntether(_ Enabled: Bool) {
-    do {
-        if Enabled {
-            FileManager.default.createFile(atPath: UntetherEnabledPath, contents: Data())
-        } else {
-            try FileManager.default.removeItem(atPath: UntetherEnabledPath)
+//From https://github.com/Odyssey-Team/Taurine/blob/main/Taurine/app/LogStream.swift
+//Code from Taurine https://github.com/Odyssey-Team/Taurine under BSD 4 License
+class LogStream {
+    static let shared = LogStream()
+    private(set) var outputString: NSMutableAttributedString = NSMutableAttributedString()
+    public let reloadNotification = Notification.Name("LogStreamReloadNotification")
+    private(set) var outputFd: [Int32] = [0, 0]
+    private(set) var errFd: [Int32] = [0, 0]
+    private let readQueue: DispatchQueue
+    private let outputSource: DispatchSourceRead
+    private let errorSource: DispatchSourceRead
+    init() {
+        readQueue = DispatchQueue(label: "org.coolstar.sileo.logstream", qos: .userInteractive, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil)
+        guard pipe(&outputFd) != -1,
+            pipe(&errFd) != -1 else {
+                fatalError("pipe failed")
         }
-    } catch {
-        print(error)
-    }
-}
-
-func UntetherInstalled() -> Bool {
-    if let analyticsdData = FileManager.default.contents(atPath: "/System/Library/PrivateFrameworks/CoreAnalytics.framework/Support/analyticsd"), let fileproviderctlData = FileManager.default.contents(atPath: "/usr/bin/fileproviderctl") {
-        return analyticsdData == fileproviderctlData
-    } else {
-        return false
+        let origOutput = dup(STDOUT_FILENO)
+        let origErr = dup(STDERR_FILENO)
+        setvbuf(stdout, nil, _IONBF, 0)
+        guard dup2(outputFd[1], STDOUT_FILENO) >= 0,
+            dup2(errFd[1], STDERR_FILENO) >= 0 else {
+                fatalError("dup2 failed")
+        }
+        outputSource = DispatchSource.makeReadSource(fileDescriptor: outputFd[0], queue: readQueue)
+        errorSource = DispatchSource.makeReadSource(fileDescriptor: errFd[0], queue: readQueue)
+        outputSource.setCancelHandler {
+            close(self.outputFd[0])
+            close(self.outputFd[1])
+        }
+        errorSource.setCancelHandler {
+            close(self.errFd[0])
+            close(self.errFd[1])
+        }
+        let bufsiz = Int(BUFSIZ)
+        outputSource.setEventHandler {
+            let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufsiz)
+            defer { buffer.deallocate() }
+            let bytesRead = read(self.outputFd[0], buffer, bufsiz)
+            guard bytesRead > 0 else {
+                if bytesRead == -1 && errno == EAGAIN {
+                    return
+                }
+                self.outputSource.cancel()
+                return
+            }
+            write(origOutput, buffer, bytesRead)
+            let array = Array(UnsafeBufferPointer(start: buffer, count: bytesRead)) + [UInt8(0)]
+            array.withUnsafeBufferPointer { ptr in
+                let str = String(cString: unsafeBitCast(ptr.baseAddress, to: UnsafePointer<CChar>.self))
+                let textColor = UIColor.white
+                let substring = NSMutableAttributedString(string: str, attributes: [NSAttributedString.Key.foregroundColor: textColor])
+                self.outputString.append(substring)
+                DispatchQueue.main.async {
+                    NotificationCenter.default.post(name: self.reloadNotification, object: nil)
+                }
+            }
+        }
+        errorSource.setEventHandler {
+            let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufsiz)
+            defer { buffer.deallocate() }
+            let bytesRead = read(self.errFd[0], buffer, bufsiz)
+            guard bytesRead > 0 else {
+                if bytesRead == -1 && errno == EAGAIN {
+                    return
+                }
+                self.errorSource.cancel()
+                return
+            }
+            write(origErr, buffer, bytesRead)
+            let array = Array(UnsafeBufferPointer(start: buffer, count: bytesRead)) + [UInt8(0)]
+            array.withUnsafeBufferPointer { ptr in
+                let str = String(cString: unsafeBitCast(ptr.baseAddress, to: UnsafePointer<CChar>.self))
+                let textColor = UIColor(red: 219/255.0, green: 44.0/255.0, blue: 56.0/255.0, alpha: 1)
+                let substring = NSMutableAttributedString(string: str, attributes: [NSAttributedString.Key.foregroundColor: textColor])
+                self.outputString.append(substring)
+                DispatchQueue.main.async {
+                    NotificationCenter.default.post(name: self.reloadNotification, object: nil)
+                }
+            }
+        }
+        outputSource.resume()
+        errorSource.resume()
     }
 }
