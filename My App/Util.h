bcca0f8343a12febde1830c6cee97a74a9af5ada
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-13 20:27:44 -0500

    Update Util.h

--- My App/Util.h
+++ My App/Util.h
@@ -1,6 +1,1 @@
 #import <Foundation/Foundation.h>
-
-NSString* getNSStringFromFile(int fd);
-NSString* spawnRoot(NSString* path, NSArray* args);
-BOOL isJailbroken(void);
-void makeExecutable(NSString* executablePath);
