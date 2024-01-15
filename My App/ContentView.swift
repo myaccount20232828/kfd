fa9105d2afc55aff27944480df6da19d35fa28ef
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-15 06:35:08 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -3,15 +3,32 @@ import SwiftUI
 struct ContentView: View {
     @State var kfd: UInt64 = 0
     @State var test = "nothing"
+    @State var LogItems: [String.SubSequence] = ["Ready!"]
     var body: some View {
         VStack {
-            Text(GetLogString())
-            Text(test)
-            .onReceive(NotificationCenter.default.publisher(for: Notification.Name("com.AppInstalleriOS.LogStream"))) { obj in
-                DispatchQueue.global(qos: .utility).async {
-                    test = "\(test)\nchanged"
+            ScrollView {
+                ScrollViewReader { scroll in
+                    VStack(alignment: .leading) {
+                        ForEach(0..<LogItems.count, id: \.self) { LogItem in
+                            Text("[*] \(String(LogItems[LogItem]))")
+                            .font(.custom("Menlo", size: 15))
+                        }
+                    }
+                    .onReceive(NotificationCenter.default.publisher(for: Notification.Name("com.AppInstalleriOS.LogStream"))) { obj in
+                        DispatchQueue.global(qos: .utility).async {
+                            test = "\(test)\nchanged"
+                            LogItems = GetLogString.split(separator: "\n")
+                            scroll.scrollTo(LogItems.count - 1)
+                        }
+                    }
                 }
+                .frame(maxWidth: .infinity, alignment: .leading)
             }
+            .padding()
+            .frame(width: UIScreen.main.bounds.width - 80, height: 300)
+            .background(Color(UIColor.systemGray6))
+            .cornerRadius(20)
+            Text(test)
             Button {
                 if kfd == 0 {
                     kfd = kopen(0x800, 0x0, 0x2, 0x2)
