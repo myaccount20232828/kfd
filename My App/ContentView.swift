50845cc1909df0b5001372a3705d3e9d03f7ea78
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-15 06:00:53 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -4,23 +4,7 @@ struct ContentView: View {
     @State var kfd: UInt64 = 0
     var body: some View {
         VStack {
-            ScrollView {
-                ScrollViewReader { scroll in
-                    Text(GetLogString())
-                    .font(.custom("Menlo", size: 15))
-                    //.onChange(of: LogString) { obj in
-                        //DispatchQueue.global(qos: .utility).async {
-                            //scroll.scrollTo(LogItems.count - 1)
-                            //UIPasteboard.general.string = "Refresh"
-                        //}
-                    //}
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            .padding()
-            .frame(width: UIScreen.main.bounds.width - 80, height: 300)
-            .background(Color(UIColor.systemGray6))
-            .cornerRadius(20)          
+            Text(GetLogString())
             Button {
                 if kfd == 0 {
                     kfd = kopen(0x800, 0x0, 0x2, 0x2)
@@ -31,7 +15,7 @@ struct ContentView: View {
                     UIPasteboard.general.string = GetLogString()
                 }
             } label: {
-                Text(kfd == 0 ? "Exploit: Log 3" : "Post Exploit")
+                Text(kfd == 0 ? "Exploit: Log 4" : "Post Exploit")
                 .font(.system(size: 20))
             }
             .buttonStyle(.plain)
