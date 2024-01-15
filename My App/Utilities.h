38a30aed0c1ce87f65d572f4a5c1df2cf0ec0a96
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-14 23:18:12 -0500

    Update Utilities.h

--- My App/Utilities.h
+++ My App/Utilities.h
@@ -3,6 +3,9 @@
 
 uint64_t kopen(uint64_t puaf_pages, uint64_t puaf_method, uint64_t kread_method, uint64_t kwrite_method);
 void kclose(uint64_t kfd);
-
 void postExploit(void);
+uint32_t kread32(uint64_t where);
+uint64_t kread64(uint64_t where);
+void kwrite32(uint64_t where, uint32_t what);
+void kwrite64(uint64_t where, uint64_t what);
 uint64_t getProc(pid_t pid);
