3e0ed512dc37b75fdad64377a251fc02ea73a5f8
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-14 23:15:03 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -2,153 +2,22 @@ import SwiftUI
 
 struct ContentView: View {
     @State var kfd: UInt64 = 0
-    @State var LogItems: [String.SubSequence] = ["Ready!"]
-    @State var ShowLog = false
     var body: some View {
-        VStack {
-            ScrollView {
-                ScrollViewReader { scroll in
-                    if ShowLog {
-                        VStack(alignment: .leading) {
-                            ForEach(0..<LogItems.count, id: \.self) { LogItem in
-                                Text("[*] \(String(LogItems[LogItem]))")
-                                //.textSelection(.enabled)
-                                .font(.custom("Menlo", size: 15))
-                            }
-                        }
-                        .onReceive(NotificationCenter.default.publisher(for: LogStream.shared.reloadNotification)) { obj in
-                            DispatchQueue.global(qos: .utility).async {
-                                FetchLog()
-                                scroll.scrollTo(LogItems.count - 1)
-                            }
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
+        Button {
+            if kfd == 0 {
+                kfd = kopen(0x800, 0x0, 0x2, 0x2)
+            } else {
+                postExploit()
+                kclose(kfd)
+                kfd = 0
             }
-            .padding()
-            .frame(width: UIScreen.main.bounds.width - 80, height: 300)
-            .background(Color(UIColor.systemGray6))
-            .cornerRadius(20)          
-            Button {
-                if kfd == 0 {
-                    kfd = kopen(0x800, 0x0, 0x2, 0x2)
-                    ShowLog = true
-                } else {
-                    print("KFD Success")
-                    ShowLog = false
-                    kclose(kfd)
-                    kfd = 0
-                }
-            } label: {
-                Text(kfd == 0 ? "Exploit 11" : "Post Exploit")
-                .font(.system(size: 20))
-            }
-            .buttonStyle(.plain)
-            .frame(width: UIScreen.main.bounds.width - 80, height: 70)
-            .background(Color(UIColor.systemGray6))
-            .cornerRadius(20)
+        } label: {
+            Text(kfd == 0 ? "Exploit 12" : "Post Exploit")
+            .font(.system(size: 20))
         }
-    }
-    func FetchLog() {
-        guard let AttributedText = LogStream.shared.outputString.copy() as? NSAttributedString else {
-            LogItems = ["Error Getting Log!"]
-            return
-        }
-        LogItems = AttributedText.string.split(separator: "\n")
-    }
-}
-
-//From https://github.com/Odyssey-Team/Taurine/blob/main/Taurine/app/LogStream.swift
-//Code from Taurine https://github.com/Odyssey-Team/Taurine under BSD 4 License
-class LogStream {
-    static let shared = LogStream()
-    private(set) var outputString: NSMutableAttributedString = NSMutableAttributedString()
-    public let reloadNotification = Notification.Name("LogStreamReloadNotification")
-    private(set) var outputFd: [Int32] = [0, 0]
-    private(set) var errFd: [Int32] = [0, 0]
-    private let readQueue: DispatchQueue
-    private let outputSource: DispatchSourceRead
-    private let errorSource: DispatchSourceRead
-    init() {
-        readQueue = DispatchQueue(label: "org.coolstar.sileo.logstream", qos: .userInteractive, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil)
-        guard pipe(&outputFd) != -1,
-            pipe(&errFd) != -1 else {
-                fatalError("pipe failed")
-        }
-        let origOutput = dup(STDOUT_FILENO)
-        let origErr = dup(STDERR_FILENO)
-        setvbuf(stdout, nil, _IONBF, 0)
-        guard dup2(outputFd[1], STDOUT_FILENO) >= 0,
-            dup2(errFd[1], STDERR_FILENO) >= 0 else {
-                fatalError("dup2 failed")
-        }
-        outputSource = DispatchSource.makeReadSource(fileDescriptor: outputFd[0], queue: readQueue)
-        errorSource = DispatchSource.makeReadSource(fileDescriptor: errFd[0], queue: readQueue)
-        outputSource.setCancelHandler {
-            close(self.outputFd[0])
-            close(self.outputFd[1])
-        }
-        errorSource.setCancelHandler {
-            close(self.errFd[0])
-            close(self.errFd[1])
-        }
-        let bufsiz = Int(BUFSIZ)
-        outputSource.setEventHandler {
-            let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufsiz)
-            defer { buffer.deallocate() }
-            let bytesRead = read(self.outputFd[0], buffer, bufsiz)
-            guard bytesRead > 0 else {
-                if bytesRead == -1 && errno == EAGAIN {
-                    return
-                }
-                self.outputSource.cancel()
-                return
-            }
-            write(origOutput, buffer, bytesRead)
-            let array = Array(UnsafeBufferPointer(start: buffer, count: bytesRead)) + [UInt8(0)]
-            array.withUnsafeBufferPointer { ptr in
-                let str = String(cString: unsafeBitCast(ptr.baseAddress, to: UnsafePointer<CChar>.self))
-                let textColor = UIColor.white
-                let substring = NSMutableAttributedString(string: str, attributes: [NSAttributedString.Key.foregroundColor: textColor])
-                self.outputString.append(substring)
-                DispatchQueue.main.async {
-                    NotificationCenter.default.post(name: self.reloadNotification, object: nil)
-                }
-            }
-        }
-        errorSource.setEventHandler {
-            let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufsiz)
-            defer { buffer.deallocate() }
-            let bytesRead = read(self.errFd[0], buffer, bufsiz)
-            guard bytesRead > 0 else {
-                if bytesRead == -1 && errno == EAGAIN {
-                    return
-                }
-                self.errorSource.cancel()
-                return
-            }
-            write(origErr, buffer, bytesRead)
-            let array = Array(UnsafeBufferPointer(start: buffer, count: bytesRead)) + [UInt8(0)]
-            array.withUnsafeBufferPointer { ptr in
-                let str = String(cString: unsafeBitCast(ptr.baseAddress, to: UnsafePointer<CChar>.self))
-                let textColor = UIColor(red: 219/255.0, green: 44.0/255.0, blue: 56.0/255.0, alpha: 1)
-                let substring = NSMutableAttributedString(string: str, attributes: [NSAttributedString.Key.foregroundColor: textColor])
-                self.outputString.append(substring)
-                DispatchQueue.main.async {
-                    NotificationCenter.default.post(name: self.reloadNotification, object: nil)
-                }
-            }
-        }
-        outputSource.resume()
-        errorSource.resume()
-    }
-    func pause() {
-        outputSource.suspend()
-        errorSource.suspend()
-    }
-    func resume() {
-        outputSource.resume()
-        errorSource.resume()
+        .buttonStyle(.plain)
+        .frame(width: UIScreen.main.bounds.width - 80, height: 70)
+        .background(Color(UIColor.systemGray6))
+        .cornerRadius(20)
     }
 }
