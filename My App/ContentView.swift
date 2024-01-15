9317aed7458c1a1d8812815e26bfac8f65dea1f9
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-15 18:06:29 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -59,67 +59,6 @@ struct ContentView: View {
     }
 }
 
-import SwiftUI
-
-struct ContentView: View {
-    @State var kfd: UInt64 = 0
-    @State var LogItems: [String.SubSequence] = ["Ready!"]
-    var body: some View {
-        VStack {
-            ScrollView {
-                ScrollViewReader { scroll in
-                    VStack(alignment: .leading) {
-                        ForEach(0..<LogItems.count, id: \.self) { LogItem in
-                            Text("[*] \(String(LogItems[LogItem]))")
-                            .font(.custom("Menlo", size: 15))
-                        }
-                    }
-                    .onChange(of: LogItems) { _ in
-                        DispatchQueue.global(qos: .utility).async {
-                            scroll.scrollTo(LogItems.count - 1)
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            .padding()
-            .frame(width: UIScreen.main.bounds.width - 80, height: 300)
-            .background(Color(UIColor.systemGray6))
-            .cornerRadius(20)
-            Button {
-                if kfd == 0 {
-                    kfd = kopen(0x800, 0x0, 0x2, 0x2)
-                } else {
-                    postExploit()
-                    kclose(kfd)
-                    kfd = 0
-                }
-            } label: {
-                Text(kfd == 0 ? "Exploit: Log 19" : "Post Exploit")
-                .font(.system(size: 20))
-            }
-            .buttonStyle(.plain)
-            .frame(width: UIScreen.main.bounds.width - 80, height: 70)
-            .background(Color(UIColor.systemGray6))
-            .cornerRadius(20)
-            Button {
-                print(Date())
-            } label: {
-                Text("Test")
-            }
-            Button {
-                postExploit()
-            } label: {
-                Text("Post Exploit")
-            }
-            .disabled(kfd == 0)
-        }
-        .onAppear {
-            LogStream($LogItems)
-        }
-    }
-}
-
 //From https://github.com/Odyssey-Team/Taurine/blob/main/Taurine/app/LogStream.swift
 //Code from Taurine https://github.com/Odyssey-Team/Taurine under BSD 4 License
 class LogStream {
